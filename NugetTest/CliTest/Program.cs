91b104b06ea97e2f5537113564684ea9225f75e7
andrei.kulikou <andrei.kulikou@soft-fx.lv>
2019-07-19 14:23:11 +0300

    Add sample test

---
+++ NugetTest/CliTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using cloud.trading.viewBusinessObjects.Container;
+using cloud.trading.viewBusinessObjects.Enum;
+using SampleLib;
+
+namespace CliTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var c = new Calculator();
+            var x = new AccountInfoContainer {AccountingType = AccountingType.Cash};
+
+            Console.WriteLine(c.Add(1, 2));
+            Console.WriteLine(x.AccountingType);
+        }
+    }
+}
